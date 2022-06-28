0b26ae3daf62bd561b0017e6dbdfd650e8452e06
Prudhvinaraya <93300939+Prudhvinaraya@users.noreply.github.com>
2022-06-28 23:11:38 +0530

    using different types of arguments can pass to function
    This is the program that creates use of arguments that pass to function.
    it is all about me.

---
+++ using python arguments.py
@@ -0,0 +1,8 @@
+#progam to find the difference b/w types of arguments
+def prudhvi(name,branch,year):
+    print("name is",name)
+    print("branch is",branch)
+    print("year is",year)
+branch="ai&ds"
+year="1st year"
+prudhvi(year,branch,name="prudhvi narayana")
